54d4a2a9a2e61cd02e5ddaff0ed944592ed8acca
FrankyPrograma <72852940+FrankyPrograma@users.noreply.github.com>
2023-10-29 19:14:06 +0100

    Add files via upload

---
+++ 02-vite-app/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+    <h1>Hola Mundo!!</h1>
+    <a href="https://vitejs.dev" target="_blank">
+      Documentación
+    </a>
+`;
